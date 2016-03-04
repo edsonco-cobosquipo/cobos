ff72bc87e65926264e0a1844af25e017e60d67ad
ronald <roal99@gamil.com>
2016-03-04 12:28:03 -0500

    creando conocimientos

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+
+	<dir class="contenedor">
+	<nav>
+		<ul class="menu-lista">
+			<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+			<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+			<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+			<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+			<li class="menu-item"><a href="" class="menu-link">CONTACTOS</a></li>
+
+		
+		</ul>		
+	</nav>	
+	<dir class="SLIDER">
+		<img src="1.jpg" alt="" height="400" width="900">
+		<img class="img" src="2.jpg" alt="" height="180" width="180">
+	  	<img class="img" src="3.jpg" alt="" height="180" width="180">
+		<img class="img" src="4.jpg" alt="" height="180" width="180">
+		<img class="img" src="5.jpg" alt="" height="180" width="180">
+		</header><!-- /header -->>
+
+	
+	</div>
+	
+</body>
+</html>
